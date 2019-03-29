896e671657db7f78a46f46f096fe5ae36f9478cc
Uma Abrami <uma.abrami@gmail.com>
2019-03-29 15:17:04 -0700

    Set up dependencies, seed script, server and database, sample get req

---
+++ database/index.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/images', { useNewUrlParser: true });
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('it worked, mongoose working!');
+});
+
+var schema = mongoose.Schema({
+  imagePath: {type: String, required: true},
+  imageId: {type: Number, required: true}
+});
+
+module.exports = mongoose.model('Image', schema);
