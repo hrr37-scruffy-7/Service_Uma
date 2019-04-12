e22f71f21de217b6c2b57435dcdb98d15bfab5d8
Uma Abrami <uma.abrami@gmail.com>
2019-04-12 15:09:08 -0700

    added static pictures to file

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -7,7 +7,7 @@ import PropTypes from 'prop-types';
 const Card = ({property}) => {
   const {imageIndex, imagePath, imageId} = property;
   return (
-    <div id={[styles.active, imageIndex].join('')} className={`${styles.card} ${[styles.activeCard, imageIndex].join('')}`}>
+    <div id={[styles.active, imageIndex].join('')} className={`${styles.card} ${[styles.active, imageIndex].join('')}`}>
       <img src={imagePath} alt={imageId} height="412px"/>
     </div>
   );
